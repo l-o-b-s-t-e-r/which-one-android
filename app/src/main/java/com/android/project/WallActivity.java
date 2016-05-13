cbe0e5dcba8d33f1f4e451a0601f0faaa1084150
Vlad <vladislav-loboda@mail.ru>
2016-05-13 22:24:25 +0200

    main recycler view

--- app/src/main/java/com/android/project/WallActivity.java
+++ app/src/main/java/com/android/project/WallActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class WallActivity extends AppCompatActivity {
 
@@ -15,6 +20,18 @@ public class WallActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wall);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        List<String> items = Arrays.asList(
+                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
+        );
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        WallRecyclerViewAdapter recyclerViewAdapter = new WallRecyclerViewAdapter(items, this);
+        recyclerView.setAdapter(recyclerViewAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
